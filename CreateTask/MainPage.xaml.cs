41f456ecdd44478f4d3d312742ea18cd5e23df72
Seadd <Seadd@LAPTOP-TEVVAD8I>
2023-11-27 16:33:48 -0500

    Add project files.

---
+++ CreateTask/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CreateTask
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+                
+        }
+
+    }
+}
